d6150bfd6cb8550175e21b097a16cb706aa5b98d
Mr-Moonrock <rstivey35@gmail.com>
2024-06-26 16:01:30 -0400

    updated enslit errors

--- server/db.js
+++ server/db.js
@@ -9,19 +9,35 @@ const pool = new Pool({
   database: process.env.DBDATABASE,
 });
 
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     console.error('Error acquiring client', err.stack);
+//   } else {
+//     release();
+//   }
+// });
+
+// pool.query('SELECT NOW()', (err, res) => {
+//   if (err) {
+//     console.error('Database connection error:', err.stack);
+//   } else {
+//     console.log('Database connected:', res.rows);
+//   }
+// });
+
 pool.connect((err, client, release) => {
   if (err) {
     console.error('Error acquiring client', err.stack);
   } else {
-    release();
-  }
-});
-
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Database connection error:', err.stack);
-  } else {
-    console.log('Database connected:', res.rows);
+    console.log('Connected to the database');
+    client.query('SELECT NOW()', (err, res) => {
+      release();
+      if (err) {
+        console.error('Database query error:', err.stack);
+      } else {
+        console.log('Database query result:', res.rows);
+      }
+    });
   }
 });
 
