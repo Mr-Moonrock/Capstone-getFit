1535a25215202eca2b8e6cb7f68082f3fe9d13a2
Mr-Moonrock <rstivey35@gmail.com>
2024-06-28 21:29:11 -0400

    updated enslit errors

--- server/db.js
+++ server/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
+  ssl: true,
 });
 
 console.log('user: ' + process.env.DB_USERNAME)
